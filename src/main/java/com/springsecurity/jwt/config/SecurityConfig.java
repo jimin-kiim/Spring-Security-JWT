08808797b01a44b461d7beff8cb1bb386af29f1f
Jimin Kim <0305jimin@gmail.com>
2024-04-10 23:51:22 +0900

    Chore: deleting redundant codes

--- src/main/java/com/springsecurity/jwt/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/jwt/config/SecurityConfig.java
@@ -1,15 +1,14 @@
 package com.springsecurity.jwt.config;
 
+import com.springsecurity.jwt.filter.MyFilter1;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -28,7 +27,6 @@ public class SecurityConfig {
                 .formLogin().disable() // not using form tag, original login style
                 .httpBasic().disable();
         http.authorizeHttpRequests(authorize -> authorize
-
                 .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "ADMIN", "MANAGER")
                 .requestMatchers("/api/v1/manager/**").hasAnyRole("ADMIN", "MANAGER")
                 .requestMatchers("/api/v1/admin/**").hasAnyRole("ADMIN")
