9b5bc0e5bdefd8106414a4d3d705c607d5449bf6
Jimin Kim <0305jimin@gmail.com>
2024-04-18 22:38:00 +0900

    Feat: authenticating the input user data

--- src/main/java/com/springsecurity/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/springsecurity/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,12 +1,21 @@
 package com.springsecurity.jwt.config.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springsecurity.jwt.config.auth.PrincipalDetails;
+import com.springsecurity.jwt.model.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 // UsernamePasswordAuthenticationFilter: when /login with username, password by POST method is requested, it works
 @RequiredArgsConstructor
@@ -17,6 +26,38 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+
+        System.out.println("JwtAuthenticationFilter");
+        try {
+
+            // ** x-www-form-urlencoded
+//            BufferedReader br = request.getReader();
+//            String input = null;
+//            while ((input = br.readLine()) != null) {
+//                System.out.println(input);
+//            }
+
+            // ** JSON
+            ObjectMapper om = new ObjectMapper();
+            User user = om.readValue(request.getInputStream(), User.class);
+
+            // creating Token
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            // authenticate with the created token
+            // loadByUsername() in PricipalDetailsService is executed.
+            // authentication: login info is contained
+            Authentication authentication
+                    = authenticationManager.authenticate(authenticationToken);
+
+            // if principalDetails.getUser().getUsername() is printed, it means the info can be brought -> login success
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+            return authentication; // then stored in session
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
